d0c42e49265b18c4bbfa1d6700e2cace7d23716d
Mohammed El Makrini <81011948+Medmakrini@users.noreply.github.com>
2022-09-23 20:44:58 +0200

    Update fakeFormationService.js

--- src/services/fakeFormationService.js
+++ src/services/fakeFormationService.js
@@ -2,22 +2,22 @@ const offers = [
     {
         name : 'C++',
         image: 'c-plus-plus.png',
-        price: 69
+        price: 60
     },
     {
         name : 'Python',
         image: 'python.png',
-        price: 69
+        price: 60
     },
     {
         name : 'Web_Dev',
         image: 'html.png',
-        price: 69
+        price: 60
     },
     {
         name : 'UI/UX_design',
         image: 'uiux.png',
-        price: 69
+        price: 60
     }
 ]
 
